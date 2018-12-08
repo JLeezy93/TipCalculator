956728aafafe8ab9c61d814cef00dde0dfb61804
Unknown <lee.josh93@gmail.com>
2018-12-07 18:12:43 -0600

    Initial commit

---
+++ TipCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TipCalculator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
