2650a2404e20a5f7b2e22d06491f0af926e08eb2
Unknown <lee.josh93@gmail.com>
2018-12-07 18:14:11 -0600

    ConsoleTipCalculator

--- TipCalculator/Program.cs
+++ TipCalculator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace TipCalculator
 {
     class MainClass
@@ -7,6 +8,22 @@ namespace TipCalculator
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //Creating a console interface for tip calculator
+            Console.WriteLine("How much is your bill?");
+
+            string subTotal = Console.ReadLine();
+            decimal convertSubTotal = Convert.ToDecimal(subTotal);
+
+            Console.WriteLine("What percentage would you like to tip?");
+
+            string tipAmount = Console.ReadLine();
+            decimal tipAmountPercent = Convert.ToDecimal(tipAmount);
+            decimal tip = tipAmountPercent * convertSubTotal;
+            decimal grandTotal = convertSubTotal + tip;
+
+            Console.WriteLine("Your tip amount is $" + tip + ".");
+            Console.WriteLine("Your grand total is $" + grandTotal + ".");
         }
     }
 }
